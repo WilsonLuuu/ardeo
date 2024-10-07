a72648eb48f8298ced0767e467a9c96f86df2fed
WilsonLuu <wilsonsonlu@gmail.com>
2024-10-07 08:33:29 +0800

    homepage blank 1.1

--- js/home-btn.js
+++ js/home-btn.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const homeButton = document.getElementById("hbtn");
+const homeButton = document.getElementById("hbtn");
     if (homeButton) {
         homeButton.addEventListener('pointermove', moveBg);
     }
@@ -9,3 +9,28 @@ document.addEventListener('DOMContentLoaded', function () {
         e.target.style.setProperty('--y', (e.clientY - rect.y) / rect.height * 100);
     }
 });
+let x;
+document.addEventListener('DOMContentLoaded', function () {
+function intro() {
+    let i = 4; 
+    let $b = document.getElementById("hbtn");
+    $b.style.setProperty( "--a", '100%' );
+    x = setInterval(() => {
+        $b.style.setProperty( 
+            "--x", -50
+        );
+        $b.style.setProperty( 
+            "--y", 50
+        );
+        i+= 0.03;
+        if( i > 11.5 ) {
+            clearInterval(x);
+            $b.style.setProperty( "--a", '' );
+        }
+    },16);
+}
+intro();
+document.querySelector("button").addEventListener('pointerover', (e) => {
+    clearInterval(x);
+    e.target.style.setProperty( "--a", '' );
+});})
